03a7d6fff797c77f9f9a5a2e50d4f74900089d8b
godl05 <154982500+godl05@users.noreply.github.com>
2025-04-02 08:36:56 +0530

    Update hamiltonian.c

--- hamiltonian.c
+++ hamiltonian.c
@@ -1,73 +1,54 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#define MAX 100
+#define MAX 20
 
-int graph[MAX][MAX];
-int colors[MAX];
-int n;
+int n, graph[MAX][MAX], path[MAX];
 
-bool isSafe(int v, int c) {
-    for (int i = 0; i < n; i++) {
-        if (graph[v][i] && colors[i] == c) {
-            return false;
-        }
-    }
+bool isSafe(int v, int pos) {
+    if (graph[path[pos - 1]][v] == 0) return false;
+    for (int i = 0; i < pos; i++)
+        if (path[i] == v) return false;
     return true;
 }
 
-bool graphColoringUtil(int m, int v) {
-    if (v == n) {
-        return true;
-    }
-
-    for (int c = 1; c <= m; c++) {
-        if (isSafe(v, c)) {
-            colors[v] = c;
-
-            if (graphColoringUtil(m, v + 1)) {
-                return true;
-            }
-
-            colors[v] = 0;
-        }
-    }
-    return false;
+void printSolution() {
+    for (int i = 0; i < n; i++)
+        printf("%d ", path[i] + 1);
+    printf("%d\n", path[0] + 1);
 }
 
-void graphColoring(int m) {
-    for (int i = 0; i < n; i++) {
-        colors[i] = 0;
-    }
-
-    if (!graphColoringUtil(m, 0)) {
-        printf("Solution does not exist\n");
+void hamiltonianCycleUtil(int pos) {
+    if (pos == n) {
+        if (graph[path[pos - 1]][path[0]] == 1)
+            printSolution();
         return;
     }
-
-    printf("Solution Exists: Assigned colors are:\n");
-    for (int i = 0; i < n; i++) {
-        printf("Vertex %d -> Color %d\n", i, colors[i]);
+    for (int v = 1; v < n; v++) {
+        if (isSafe(v, pos)) {
+            path[pos] = v;
+            hamiltonianCycleUtil(pos + 1);
+            path[pos] = -1;
+        }
     }
+}
+
+void findHamiltonianCycles() {
+    for (int i = 0; i < n; i++) path[i] = -1;
+    path[0] = 0;
+    hamiltonianCycleUtil(1);
 }
 
 int main() {
-    int m;
-
-    printf("Enter the number of vertices: ");
+    printf("Enter number of vertices: ");
     scanf("%d", &n);
-
-    printf("Enter the adjacency matrix (%dx%d):\n", n, n);
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+    printf("Enter adjacency matrix:\n");
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
             scanf("%d", &graph[i][j]);
-        }
-    }
 
-    printf("Enter the number of colors: ");
-    scanf("%d", &m);
-
-    graphColoring(m);
-
+    printf("Hamiltonian Cycles:\n");
+    findHamiltonianCycles();
+   
     return 0;
 }
