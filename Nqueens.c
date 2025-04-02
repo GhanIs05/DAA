02c96e7936ecedbed4f2288fb42a9b14f67c260d
godl05 <154982500+godl05@users.noreply.github.com>
2025-04-02 08:53:20 +0530

    Update Nqueens.c

--- Nqueens.c
+++ Nqueens.c
@@ -1,1 +1,42 @@
+include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
+int x[100];
+
+int place(int k, int i) {
+    for (int j = 1; j < k; j++) {
+        if (x[j] == i || abs(x[j] - i) == abs(j - k)) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+
+void nqueens(int k, int n) {
+    for (int i = 1; i <= n; i++) {
+        if (place(k, i)) {
+            x[k] = i;  // Place the queen
+            if (k == n) {
+                for (int j = 1; j <= n; j++) {
+                    printf("%d ", x[j]);
+                }
+                printf("\n");
+            } else {
+                nqueens(k + 1, n);  // Try placing the next queen
+            }
+        }
+    }
+}
+
+int main() {
+    int n;
+
+    printf("Enter the value of n (size of the chessboard): ");
+    scanf("%d", &n);
+    printf("Solutions are: \n");
+    nqueens(1, n);
+
+    return 0;
+}
